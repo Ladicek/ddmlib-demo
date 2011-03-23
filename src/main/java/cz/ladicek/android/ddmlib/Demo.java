5ecbaab1939dfc402f451fa40a67c0621f0a271d
Ladislav Thon <ladicek@gmail.com>
2011-03-23 23:09:29 +0100

    Simple demo to show basic usage of Android Tools ddmlib.

---
+++ src/main/java/cz/ladicek/android/ddmlib/Demo.java
@@ -0,0 +1,80 @@
+package cz.ladicek.android.ddmlib;
+
+import com.android.ddmlib.AndroidDebugBridge;
+import com.android.ddmlib.IDevice;
+
+public class Demo {
+    // you can call AndroidDebugBridge.init() and terminate() only once
+    // createBridge() and disconnectBridge() can be called as many times as you want
+
+    public void init() {
+        AndroidDebugBridge.init(false);
+    }
+
+    public void finish() {
+        AndroidDebugBridge.terminate();
+    }
+
+    public void usingWaitLoop() throws Exception {
+        AndroidDebugBridge adb = AndroidDebugBridge.createBridge();
+
+        int trials = 10;
+        while (trials > 0) {
+            Thread.sleep(50);
+            if (adb.isConnected()) {
+                break;
+            }
+            trials--;
+        }
+
+        if (!adb.isConnected()) {
+            System.out.println("Couldn't connect to ADB server");
+        } else {
+            for (IDevice device : adb.getDevices()) {
+                System.out.println("- " + device.getSerialNumber());
+            }
+        }
+
+        AndroidDebugBridge.disconnectBridge();
+    }
+
+    public void usingDeviceChangeListener() throws Exception {
+        AndroidDebugBridge.addDeviceChangeListener(new AndroidDebugBridge.IDeviceChangeListener() {
+            // this gets invoked on another thread, but you probably shouldn't count on it
+            public void deviceConnected(IDevice device) {
+                System.out.println("* " + device.getSerialNumber());
+            }
+
+            public void deviceDisconnected(IDevice device) {
+            }
+
+            public void deviceChanged(IDevice device, int changeMask) {
+            }
+        });
+
+        AndroidDebugBridge adb = AndroidDebugBridge.createBridge();
+
+        Thread.sleep(1000);
+        if (!adb.isConnected()) {
+            System.out.println("Couldn't connect to ADB server");
+        }
+
+        AndroidDebugBridge.disconnectBridge();
+    }
+
+    public static void main(String[] args) throws Exception {
+        Demo demo = new Demo();
+
+        demo.init();
+
+        // I think this is the way to go for non-interactive or short-running applications
+        System.out.println("Demo using wait loop to ensure connection to ADB server and then enumerate devices synchronously");
+        demo.usingWaitLoop();
+
+        // this looks like the right way for interactive or long-running applications
+        System.out.println("Demo using DeviceChangeListener to get information about devices asynchronously");
+        demo.usingDeviceChangeListener();
+
+        demo.finish();
+    }
+}
---
+++ src/readme.txt
@@ -0,0 +1,8 @@
+This is a demo of how to connect to ADB server using ddmlib and enumerate connected Android devices.
+
+If you want to run it, you can easily compile it yourself or you can use Gradle like this:
+
+1. Install Gradle, at least 1.0-milestone-1. See http://www.gradle.org.
+2. Run the application with 'gradle run'.
+
+Note that the ADB server should be running. It it isn't, it is detected and the program fails. This is on purpose.
